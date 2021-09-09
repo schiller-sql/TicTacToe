2f53d90d6c01aaa9cb39eadf6b6b6f3e0aca4e89
Lennart Protte <lenni.protte@gmx.de>
2021-09-09 19:56:51 +0200

    Change .gitignore

---
+++ GameLogic/src/main/java/opponent/ExampleOpponent.java
@@ -0,0 +1,13 @@
+package opponent;
+
+import domain.Grid;
+import domain.Point;
+
+public class ExampleOpponent implements Opponent{
+
+
+    @Override
+    public Point move(Grid grid) {
+        return null;
+    }
+}
