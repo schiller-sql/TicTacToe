44c16e78e793741c1ae29d8f6348add971f4139c
Lennart Protte <lenni.protte@gmx.de>
2021-09-14 14:47:57 +0200

    Main.java

--- ConsoleApp/src/main/java/Main.java
+++ ConsoleApp/src/main/java/Main.java
@@ -5,13 +5,5 @@ import opponent.Opponent;
 public class Main {
 
     public static void main(String[] args) {
-        Opponent op = new Opponent() {
-            @Override
-            public Point move(Grid grid) {
-                return null;
-            }
-        };
-        System.out.println(op);
-        // Load CLI app
     }
 }
