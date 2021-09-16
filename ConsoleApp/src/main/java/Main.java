309ed4371beada39a2d1931526da75d51c8c2bb7
Lennart Protte <lenni.protte@gmx.de>
2021-09-16 23:46:53 +0200

    Improve the console feedback in Main.java Add some TODOs

--- ConsoleApp/src/main/java/Main.java
+++ ConsoleApp/src/main/java/Main.java
@@ -1,5 +1,7 @@
 import controller.GameController;
 import controller.GameState;
+import controller.GridHistory;
+import domain.Grid;
 import domain.Mark;
 import domain.Point;
 import opponent.ExampleOpponent;
@@ -9,20 +11,50 @@ import utils.TerminalColors;
 import utils.TerminalUtils;
 import utils.TicTacToeUtils;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.stream.IntStream;
 
 
 public class Main {
-
     static GameController controller;
 
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe, select a opponent: ");
-        final Opponent opponent = getOpponentInput();
-        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
+        System.out.println("Welcome to TicTacToe");
+        System.out.println("\nSelect the number of the opponent you want to play against:");
 
+        boolean status = true;
+        final Opponent opponent = getOpponentInput();
         controller = new GameController(opponent);
 
+        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
+        printTutorial();
+
+         do {
+            game();
+        } while(status);
+    }
+
+    private static void game() {
+
+        while (controller.getState() == GameState.running) {
+            final Point point = getPointInput();
+            controller.setPoint(point);
+            System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
+        }
+        System.out.println(TicTacToeUtils.gameStateToString(controller.getState())+ "\r\n");
+
+        //TODO: Check if the player wants to continue (Else press Q)
+        //TODO: add the option to print the game history
+
+        System.out.println("Select a opponent: ");
+        final Opponent opponent = getOpponentInput();
+        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
+        controller = new GameController(opponent);
+        System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
+    }
+
+    private static void printTutorial() {
         System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
 
         System.out.println("Press the number key of the respective field");
@@ -37,24 +69,28 @@ public class Main {
         System.out.print(TerminalUtils.colorString("1", TerminalColors.black) + " and ");
         System.out.print(TerminalUtils.colorString("9", TerminalColors.black));
         System.out.println(" to select a field:");
-
-        while (controller.getState() == GameState.running) {
-            final Point point = getPointInput();
-            controller.setPoint(point);
-            System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
-        }
-        System.out.println(TicTacToeUtils.gameStateToString(controller.getState()));
     }
+
+
+    @SuppressWarnings("unused")
+    private static void printHistory() {
+        final GridHistory history = controller.getHistory();
+        for (int i = 0; i < history.getLength(); i++) {
+            final Grid grid = history.getHistoryRecord(i);
+            System.out.println(TicTacToeUtils.gridToString(grid));
+        }
+    }
+
 
     private static Opponent getOpponentInput() {
         final Opponent[] opponents = new Opponent[]{
                 new RandomOpponent(),
                 new ExampleOpponent(),
         };
+
         for (int i = 0; i < opponents.length; i++) {
             System.out.println(i + 1 + ": " + opponents[i].getClass().getSimpleName());
         }
-        System.out.println("\nSelect the number of the opponent you want to play against:");
         final int choice = TerminalUtils.getIntInput(1, opponents.length);
         return opponents[choice - 1];
     }
