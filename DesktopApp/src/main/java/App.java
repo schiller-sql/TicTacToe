b5ce1cc1af852ec9db9cf798fcdea6322a13ff03
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-09-16 16:33:26 +0200

    Fix maven configuation

--- DesktopApp/src/main/java/App.java
+++ DesktopApp/src/main/java/App.java
@@ -1,10 +1,5 @@
-import opponent.ExampleOpponent;
-import opponent.Opponent;
-
 public class App {
     public static void main(String[] args) {
-        final Opponent op = new ExampleOpponent();
-        System.out.println(op);
+        // implement main method
     }
 }
-
