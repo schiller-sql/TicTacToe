435ae53dc16085e6191c8534c8524256464b6981
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-09-17 23:03:55 +0200

    New ConsoleApp structure implemented
    Each Game is represented by the Game class, and you can quit, restart or go back to the menu anywhere

--- ConsoleApp/src/main/java/Main.java
+++ ConsoleApp/src/main/java/Main.java
@@ -1,119 +1,75 @@
-import controller.GameController;
-import controller.GameState;
 import controller.GridHistory;
-import domain.Grid;
-import domain.Mark;
-import domain.Point;
-import opponent.ExampleOpponent;
-import opponent.Opponent;
-import opponent.RandomOpponent;
+import exceptions.TicTacToeMenuException;
+import exceptions.TicTacToeQuitException;
+import exceptions.TicTacToeRestartException;
 import utils.TerminalColors;
 import utils.TerminalUtils;
 import utils.TicTacToeUtils;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.stream.IntStream;
-
+import java.util.Scanner;
 
 public class Main {
-    static GameController controller;
-
     public static void main(String[] args) {
-        System.out.println("Welcome to TicTacToe");
-        System.out.println("\nSelect the number of the opponent you want to play against:");
-
-        boolean status = true;
-        final Opponent opponent = getOpponentInput();
-        controller = new GameController(opponent);
-
-        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
-        printTutorial();
-
-         do {
-            game();
-        } while(status);
-    }
-
-    private static void game() {
-
-        while (controller.getState() == GameState.running) {
-            final Point point = getPointInput();
-            controller.setPoint(point);
-            System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
-        }
-        System.out.println(TicTacToeUtils.gameStateToString(controller.getState())+ "\r\n");
-
-        //TODO: Check if the player wants to continue (Else press Q)
-        //TODO: add the option to print the game history
-
-        System.out.println("Select a opponent: ");
-        final Opponent opponent = getOpponentInput();
-        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
-        controller = new GameController(opponent);
-        System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
-    }
-
-    private static void printTutorial() {
-        System.out.println(TicTacToeUtils.gridToString(controller.getGrid()));
-
-        System.out.println("Press the number key of the respective field");
-        System.out.println("you want to place your cross on");
+        TerminalUtils.printColor("Welcome to TicTacToe", TerminalColors.purple);
         System.out.println();
 
-        System.out.println("You are: " + TicTacToeUtils.markToString(Mark.self));
-        System.out.println("And the opponent is: " + TicTacToeUtils.markToString(Mark.opponent));
-        System.out.println();
+        TerminalUtils.printColor("You are now in the main menu, here are the commands:", TerminalColors.cyan);
+        listCommands();
 
-        System.out.print("Please type a number between ");
-        System.out.print(TerminalUtils.colorString("1", TerminalColors.black) + " and ");
-        System.out.print(TerminalUtils.colorString("9", TerminalColors.black));
-        System.out.println(" to select a field:");
-    }
-
-
-    @SuppressWarnings("unused")
-    private static void printHistory() {
-        final GridHistory history = controller.getHistory();
-        for (int i = 0; i < history.getLength(); i++) {
-            final Grid grid = history.getHistoryRecord(i);
-            System.out.println(TicTacToeUtils.gridToString(grid));
+        try {
+            matchCommands();
+        } catch (TicTacToeQuitException e) {
+            TerminalUtils.printColor("Program successfully exited", TerminalColors.cyan);
         }
     }
 
-
-    private static Opponent getOpponentInput() {
-        final Opponent[] opponents = new Opponent[]{
-                new RandomOpponent(),
-                new ExampleOpponent(),
-        };
-
-        for (int i = 0; i < opponents.length; i++) {
-            System.out.println(i + 1 + ": " + opponents[i].getClass().getSimpleName());
+    private static GridHistory startGame() throws TicTacToeQuitException {
+        try {
+            final Game game = new Game();
+            TerminalUtils.printColor("You are now back in the main menu", TerminalColors.cyan);
+            return game.getHistory();
+        } catch (TicTacToeMenuException e) {
+            TerminalUtils.printColor("You are now in the main menu", TerminalColors.cyan);
+            return null;
+        } catch (TicTacToeRestartException e) {
+            TerminalUtils.printColor("Game successfully restarted", TerminalColors.cyan);
+            return startGame();
         }
-        final int choice = TerminalUtils.getIntInput(1, opponents.length);
-        return opponents[choice - 1];
     }
 
-    private static Point getPointInput() {
-        int count = 0;
-        HashMap<Integer, Point> map = new HashMap<>();
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                count++;
-                map.put(count, new Point(x, y));
-            }
-        }
-
-        int input = -1;
-        do {
-            if (input == -1) {
-                System.out.println("Choose your Point:");
+    private static void matchCommands() throws TicTacToeQuitException {
+        GridHistory lastHistory = null;
+        while (true) {
+            final Scanner scanner = new Scanner(System.in);
+            final String command = TerminalUtils.getInputWithExits(scanner);
+            if (command.matches("^:g(game)?$")) {
+                lastHistory = startGame();
+            } else if (command.matches("^:h(istory)?$")) {
+                if (lastHistory != null) {
+                    System.out.println(TicTacToeUtils.gridHistoryToString(lastHistory));
+                } else {
+                    System.out.println("There is no game, to print the history from");
+                }
+            } else if (command.matches("^:c(commands)?$")) {
+                TerminalUtils.printColor("Commands:", TerminalColors.cyan);
+                listCommands();
+            } else if (command.matches("^:")) {
+                TerminalUtils.printError("Not a valid command, for all valid commands see :c(ommands)");
             } else {
-                System.err.println("The point was already taken, choose a new one:");
+                TerminalUtils.printError(
+                        "All commands have to be prefixed with ':', " +
+                        "for all valid commands see :c(ommands)\""
+                );
             }
-            input = TerminalUtils.getIntInput(1, 9);
-        } while (!controller.getGrid().markIsEmpty(map.get(input)));
-        return map.get(input);
+        }
+    }
+
+    private static void listCommands() {
+        System.out.println(TerminalUtils.colorString("  :q(uit)", TerminalColors.blue) + ",        quit the program");
+        System.out.println(TerminalUtils.colorString("  :m(enu)", TerminalColors.blue) + ",        go back to the menu         (only in a game)");
+        System.out.println(TerminalUtils.colorString("  :r(estart)", TerminalColors.blue) + ",     restart the game            (only in a game)");
+        System.out.println(TerminalUtils.colorString("  :g(game)", TerminalColors.blue) + ",       start a new game            (only in the main menu)");
+        System.out.println(TerminalUtils.colorString("  :h(history)", TerminalColors.blue) + ",    show history of last game   (only in the main menu, after a game)");
+        System.out.println(TerminalUtils.colorString("  :c(commands)", TerminalColors.blue) + ",   list all commands           (only in the main menu)");
     }
 }
