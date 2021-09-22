7dea1777ee068493899635214135d8651123b045
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-09-22 19:33:33 +0200

    Feature/game logic/new grid history method (#30)
    * added getActorTOHistoryRecord() method to GridHistory
    
    * Tests for getActorToHistoryRecord method
    
    Co-authored-by: Olevenbaum <oh.deifuss@gmx.de>

--- GameLogic/src/main/java/controller/GridHistory.java
+++ GameLogic/src/main/java/controller/GridHistory.java
@@ -1,6 +1,7 @@
 package controller;
 
 import domain.Grid;
+import domain.Mark;
 
 import java.util.List;
 
@@ -27,5 +28,26 @@ public record GridHistory(List<Grid> gridList) {
         return gridList.get(historyPosition);
     }
 
-    //TODO: +getActorToHistoryRecord(int historyPosition): Mark
+    /**
+     * @param historyPosition The position of the grid in the timeline,
+     *                        the first Grid placed has the history position of 0.
+     *                        <p>
+     *                        Make sure that the historyPosition is at least 1,
+     *                        as it has to be compared with the Grid before it
+     * @return The Mark type of the last placed mark (in history)
+     */
+    public Mark getActorToHistoryRecord(int historyPosition) {
+
+        Grid firstGrid = getHistoryRecord(historyPosition);
+        Grid secondGrid = getHistoryRecord(historyPosition - 1);
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (firstGrid.getMark(i, j) != secondGrid.getMark(i, j)) {
+                    return firstGrid.getMark(i, j);
+                }
+            }
+        }
+        return null;
+    }
 }
