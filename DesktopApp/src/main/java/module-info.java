1b304f720aef9f406e5ebd29dbc5c267fbc3ce13
Lennart Protte <lenni.protte@gmx.de>
2021-09-27 16:26:25 +0200

    Make DesktopApp as FXML-Module, Refresh Module Setup

---
+++ DesktopApp/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module DesktopApp {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens application to javafx.fxml;
+    opens controller to javafx.fxml;
+    exports application;
+    exports controller;
+}
