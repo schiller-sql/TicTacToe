68ef6ad46138d4df15a18105d957098b3e5ba704
Lennart Protte <lenni.protte@gmx.de>
2021-09-30 16:53:35 +0200

    Progress in Menu

--- DesktopApp/src/main/java/controller/scene/LoadingSceneController.java
+++ DesktopApp/src/main/java/controller/scene/LoadingSceneController.java
@@ -1,43 +1,24 @@
 package controller.scene;
 
-import controller.GameController;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoadingSceneController {
 
-    @FXML
-    Button button;
-
-
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
-
-    public void selectButton(ActionEvent e) throws IOException {
-        Circle circle = new Circle(15);
-        this.button = (Button) e.getSource();
-        button.setShape(circle);
-    }
-
     public void switchToMainScene(ActionEvent e) throws IOException {
-
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/content/main-scene.fxml"));
-        root = loader.load();
+        Parent root = loader.load();
 
-        //MainSceneController controller = loader.getController(); to give attributes to the mainscenecontroller
+        //MainSceneController controller = loader.getController(); to give attributes to the MainSceneController
 
-        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
-        scene = new Scene(root);
+        Stage stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
--- DesktopApp/src/main/java/controller/scene/MainSceneController.java
+++ DesktopApp/src/main/java/controller/scene/MainSceneController.java
@@ -1,43 +1,102 @@
 package controller.scene;
 
 import controller.GameController;
+import domain.Point;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.RadioMenuItem;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
-import javafx.scene.shape.Circle;
 import opponent.Opponent;
-import opponent.default_opponents.MinimaxOpponent;
 
-import java.io.IOException;
+import java.util.HashMap;
 
 public class MainSceneController {
 
-    @FXML
-    Button button;
-
-    /**
-     * First select a RiadioMenueItem
-     * Then set the current opponent to the radiomenueitem
-     * HashMap with radiomenueitems and opponents
-     */
-
-    private GameController controller = new GameController(new MinimaxOpponent());
+    private Button button;
+    private GameController controller;
     private Opponent opponent;
+    private ImageView imageView;
+    private HashMap<String, Opponent> opponentClasses = new HashMap();
+    private HashMap<Button, Point> field = new HashMap();
 
-    public void selectButton(ActionEvent e) throws IOException {
-        ImageView imageView = new ImageView(getClass().getResource("/images/cross.png").toExternalForm());
+    @FXML
+    RadioMenuItem RandomOpponent = new RadioMenuItem(); //Default Opponent
+
+    @FXML
+    ToggleGroup opponents = new ToggleGroup();
+
+    @FXML
+    MenuItem start, restart, surrender;
+
+    public MainSceneController(){
+        final Opponent[] availableOpponents = Opponent.defaultOpponents();
+        for (int i = 0; i < availableOpponents.length; i++) {
+            opponentClasses.put(availableOpponents[i].getClass().getSimpleName(), availableOpponents[i]);
+        }
+
+        opponent = opponentClasses.get(RandomOpponent.toString());
+        controller = new GameController(opponent);
+    }
+
+    @FXML
+    public void initialize() {
+        imageView = new ImageView(getClass().getResource("/images/cross.png").toExternalForm());
         imageView.setFitHeight(50);
         imageView.setFitWidth(50);
+
+        RandomOpponent.setToggleGroup(opponents);
+        RandomOpponent.setSelected(true);
+    }
+
+    public void selectButton(ActionEvent e){ //TODO
         this.button = (Button) e.getSource();
         button.setGraphic(imageView);
     }
 
-    public void selectOpponent(ActionEvent e) {
-        opponent = (Opponent) e.getSource();
+    public void selectOpponent(ActionEvent e) { //TODO should available in a running game?
+        String pOpponent = ((MenuItem) e.getSource()).getText();
+        opponent = opponentClasses.get(pOpponent);
         controller = new GameController(opponent);
-        System.out.println("The opponent \"" + opponent.getClass().getSimpleName() + "\" was chosen\n");
+    }
+
+    public void startGame() {
+        start.setStyle("-fx-text-fill:gray");
+        start.setDisable(true);
+
+        restart.setDisable(false);
+        restart.setStyle("-fx-text-fill:black");
+
+        surrender.setDisable(false);
+        surrender.setStyle("-fx-text-fill:black");
+
+        //TODO: clear the field
+    }
+
+    public void restartGame() {
+        start.setDisable(false);
+        start.setStyle("-fx-text-fill:black");
+
+        restart.setDisable(false);
+        restart.setStyle("-fx-text-fill:black");
+
+        surrender.setDisable(false);
+        surrender.setStyle("-fx-text-fill:black");
+
+        //TODO: clear the field
+    }
+
+
+    public void surrenderGame() {
+        start.setDisable(false);
+        start.setStyle("-fx-text-fill:black");
+
+        restart.setDisable(true);
+        restart.setStyle("-fx-text-fill:gray");
+
+        surrender.setDisable(true);
+        surrender.setStyle("-fx-text-fill:gray");
     }
 }
