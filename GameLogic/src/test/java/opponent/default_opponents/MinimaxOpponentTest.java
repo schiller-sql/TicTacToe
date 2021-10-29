2c6350c93022de4e053497be9d672cec4380d3e1
bit-burger <65500763+bit-burger@users.noreply.github.com>
2021-10-29 20:50:17 +0200

    Tests to check if the MinimaxOpponent will always win or tie implemented (#46)

--- GameLogic/src/test/java/opponent/default_opponents/MinimaxOpponentTest.java
+++ GameLogic/src/test/java/opponent/default_opponents/MinimaxOpponentTest.java
@@ -1,12 +1,15 @@
 package opponent.default_opponents;
 
+import controller.GameController;
+import controller.GameState;
+import domain.Grid;
 import domain.Point;
 import opponent.Opponent;
 import opponent.OpponentTest;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static util.GridUtils.getGridFromString;
 
 class MinimaxOpponentTest extends OpponentTest {
@@ -103,5 +106,51 @@ class MinimaxOpponentTest extends OpponentTest {
     @Override
     protected Opponent getOpponent() {
         return new MinimaxOpponent();
+    }
+
+
+    @Test
+    public void alwaysWinsOrTies() {
+        callOtherMoves(new Grid(), new Grid[0]);
+    }
+
+    private void makeOneMove(Point point, Grid grid, Grid[] gridHistory) {
+        final var controller = new GameController(opponent, grid);
+        controller.setPoint(point);
+
+        if (controller.getState() == GameState.running) {
+            final var newGrid = controller.getGrid();
+            final var newGridHistory = addOneToGridHistory(gridHistory, newGrid);
+            callOtherMoves(newGrid, newGridHistory);
+        } else {
+            final var lastHistory = addOneToGridHistory(gridHistory, controller.getGrid());
+            assertNotEquals(
+                    controller.getState(),
+                    GameState.won,
+                    "\nThis scenario has been lost: \n" + gridHistoryToString(lastHistory)
+            );
+        }
+    }
+
+    private void callOtherMoves(Grid grid, Grid[] gridHistory) {
+        for (var point : grid.getAllMarkPositions(null)) {
+            makeOneMove(point, grid, gridHistory);
+        }
+    }
+
+    private Grid[] addOneToGridHistory(Grid[] history, Grid next) {
+        final var newHistory = new Grid[history.length + 1];
+        System.arraycopy(history, 0, newHistory, 0, history.length);
+        newHistory[newHistory.length - 1] = next;
+        return newHistory;
+    }
+
+    private String gridHistoryToString(Grid[] history) {
+        StringBuilder s = new StringBuilder();
+        for (var grid : history) {
+            s.append(grid.toString("  "));
+            s.append("\n");
+        }
+        return s.toString();
     }
 }
