bf98100a0388f3f2299cb1cb129b905afa19454b
Lennart Protte <lenni.protte@gmx.de>
2021-10-30 12:26:59 +0200

    Add CellFactorys

--- DesktopApp/src/main/java/controller/scene/MainSceneController.java
+++ DesktopApp/src/main/java/controller/scene/MainSceneController.java
@@ -2,7 +2,6 @@ package controller.scene;
 
 import controller.GameController;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -10,10 +9,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import javafx.util.Callback;
 import opponent.Opponent;
 import opponent.default_opponents.RandomOpponent;
 import persistence.GameRecord;
+import persistence.GameRecordStorageException;
 import persistence.SQLitePersistentGameRecordStorage;
 
 import java.io.IOException;
@@ -26,7 +27,9 @@ public class MainSceneController {
     private final GameController controller;
     private Opponent opponent;
     private final HashMap<String, Opponent> opponentClasses = new HashMap();
-    SQLitePersistentGameRecordStorage storage;
+    private SQLitePersistentGameRecordStorage storage;
+    private MenuItem showHistory, playGame, deleteGame;
+    ContextMenu contextMenu;
 
     @FXML
     RadioMenuItem RandomOpponent = new RadioMenuItem(); //Default Opponent
@@ -64,6 +67,36 @@ public class MainSceneController {
         RandomOpponent.setSelected(true);
         updateList();
         updateScores();
+
+        // Create MenuItems and place them in a ContextMenu
+        showHistory = new MenuItem("show history");
+        playGame = new MenuItem("play");
+        deleteGame = new MenuItem("delete");
+        contextMenu = new ContextMenu(showHistory, playGame, deleteGame);
+        // sets a cell factory on the ListView telling it to use the previously-created ContextMenu (uses default cell factory)
+        listGames.setCellFactory(ContextMenuListCell.forListView(contextMenu));
+        //set the actions to the MenuItems
+        showHistory.setOnAction(e -> {
+            showHistory((GameRecord) listGames.getSelectionModel().getSelectedItem());
+        });
+        playGame.setOnAction(e -> {
+            try {
+                playGame((GameRecord) listGames.getSelectionModel().getSelectedItem());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
+        deleteGame.setOnAction(e -> {
+            try {
+                storage.deleteGameRecord((GameRecord) listGames.getSelectionModel().getSelectedItem());
+            } catch (GameRecordStorageException ex) {
+                ex.printStackTrace();
+            }
+        });
+    }
+
+    private void showHistory(GameRecord gameRecord) {
+        //shows the game history
     }
 
     public List<String> gameRecordsToString() {
@@ -98,10 +131,31 @@ public class MainSceneController {
         stage.show();
     }
 
-    public void updateList() {
+    public void playGame(GameRecord gameRecord) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/content/game-scene.fxml"));
+        Parent root = loader.load();
+
+        GameSceneController gameSceneController = loader.getController();
+        gameSceneController.setStorage(storage);
+        gameSceneController.setController(gameRecord.getController());
+
+        Stage stage = (Stage) Stage.getWindows().stream().filter(Window::isShowing).findFirst().orElse(null);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /*public void updateList() {
         listGames.getItems().clear();
         for(String s : gameRecordsToString()) {
             listGames.getItems().add(s);
+        }
+    }*/
+
+    public void updateList() {
+        listGames.getItems().clear();
+        for(GameRecord record : storage.getCachedGameRecords()) {
+            listGames.getItems().add(record);
         }
     }
 
@@ -144,9 +198,11 @@ public class MainSceneController {
     }
 
     public static class ContextMenuListCell<T> extends ListCell<T> {
+
         public static <T> Callback<ListView<T>,ListCell<T>> forListView(ContextMenu contextMenu) {
             return forListView(contextMenu, null);
         }
+
         public static <T> Callback<ListView<T>,ListCell<T>> forListView(final ContextMenu contextMenu, final Callback<ListView<T>, ListCell<T>> cellFactory) {
             return listView -> {
                 ListCell<T> cell = cellFactory == null ? new DefaultListCell<>() : cellFactory.call(listView);
@@ -154,9 +210,11 @@ public class MainSceneController {
                 return cell;
             };
         }
+
         public ContextMenuListCell(ContextMenu contextMenu) {
             setContextMenu(contextMenu);
         }
+
     }
 
     public static class DefaultListCell<T> extends ListCell<T> {
@@ -176,19 +234,6 @@ public class MainSceneController {
                 setGraphic(null);
             }
         }
-    }
 
-    public void execute() {
-        ListView<String> listView = new ListView<>();
-        // Create a MenuItem and place it in a ContextMenu
-        MenuItem helloWorld = new MenuItem("Hello World!");
-        ContextMenu contextMenu = new ContextMenu(helloWorld);
-
-        // sets a cell factory on the ListView telling it to use the previously-created ContextMenu (uses default cell factory)
-        listView.setCellFactory(ContextMenuListCell.forListView(contextMenu));
-
-        helloWorld.setOnAction(
-                e -> System.out.println("Selected item: " + listView.getSelectionModel().getSelectedItem())
-        );
     }
 }
